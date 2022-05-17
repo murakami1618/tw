456d3c83e13e5411f45f685a19874d95169a27c9
suguru-gbgg <takagisn12@gmail.com>
2022-05-17 17:05:24 +0900

    ブートストラップの追加

--- top.php
+++ top.php
@@ -1,8 +1,17 @@
 <html>
 
-    <form action="action.php" method="GET">
-        <input type="text" name="user_id">
-        <input type="submit">
-    </form>
+    <head>
+            <link href="css/bootstrap.min.css" rel="stylesheet">
+            <meta charset="utf-8">
+            <title>SNS</title>
+    </head>
+
+    
+    <div class="text-center">
+        <form action="action.php" method="GEt">    
+            <input type="text" name="text1" class="m-3" placeholder="テキストを入れてください">
+            <input class="btn btn-outline-primary" type="submit">
+        </form>    
+    </div>   
 
 </html>
