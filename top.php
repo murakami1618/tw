e0b08775549dc0829120beb9dd3e31cfb02809de
suguru-gbgg <takagisn12@gmail.com>
2022-05-17 19:25:02 +0900

    ちょっと変更しました

--- top.php
+++ top.php
@@ -3,13 +3,13 @@
     <head>
             <link href="css/bootstrap.min.css" rel="stylesheet">
             <meta charset="utf-8">
-            <title>SNS</title>
+            <title>birthday</title>
     </head>
 
-    
+
     <div class="text-center">
         <form action="action.php" method="GEt">    
-            <input type="text" name="text1" class="m-3" placeholder="テキストを入れてください">
+            <input type="text" name="user_id" class="m-3" placeholder="TwitterIDを入れてください">
             <input class="btn btn-outline-primary" type="submit">
         </form>    
     </div>   
