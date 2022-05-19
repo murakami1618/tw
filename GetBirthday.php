6e8510d1bc49959582b3d80fd67aa74d6f100eec
murato1618 <murato.oic@gmail.com>
2022-05-19 20:54:53 +0900

    testdate

--- GetBirthday.php
+++ GetBirthday.php
@@ -1,7 +1,12 @@
 <?php
     class BirthdayUser{
         function GetBirthdayUser(){
-            $user=['choco_sab115','s__sa_m,UNO_offline','ku_Lan17,KisaragiAsh','K98_25,Cereale_1129','hime_turu1','Yazawa_GRB','RETE__Ex_'];
+            $user = [
+                ['user_id'=>'choco_sab115','birthday'=>'1/1'],
+                ['user_id'=>'s__sa_m','birthday'=>'1/2'],
+                ['user_id'=>'UNO_offline','birthday'=>'1/3'],
+                ['user_id'=>'ku_Lan17','birthday'=>'1/4'],
+            ];
             return $user;
         }
     }
