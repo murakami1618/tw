e60c0efd39532db31adf9e670abbbbf1f357fd56
murato1618 <murato.oic@gmail.com>
2022-05-19 16:53:42 +0900

    next_tokenを取得して連続でフォロワー取得

--- action.php
+++ action.php
@@ -42,10 +42,39 @@
 $id = $array['data'][0]["id"];
 
 $request_url = 'https://api.twitter.com/2/users/'.$id.'/following';	
+
+
+
+
+// パラメータ (オプション)
+$params = array(
+
+	//"usernames" => $_GET["user_id"],
+	"max_results" => '20',
+
+
+) ;
+
+// パラメータがある場合
+if( $params ) {
+	$request_url .= '?' . http_build_query( $params ) ;
+}
+
+// リクエスト用のコンテキスト
+$context = array(
+	'http' => array(
+		'method' => 'GET' , // リクエストメソッド
+		'header' => array(			  // ヘッダー
+			'Authorization: Bearer ' . $bearer_token ,
+		) ,
+	) ,
+) ;
+
 	// cURLを使ってリクエスト
 	$curl = curl_init() ;
 	curl_setopt( $curl , CURLOPT_URL , $request_url ) ;
 	curl_setopt( $curl , CURLOPT_HEADER, 1 ) ; 
+	var_dump($request_url);
 	curl_setopt( $curl , CURLOPT_RETURNTRANSFER , true ) ;								// curl_execの結果を文字列で返す
 	curl_setopt( $curl , CURLOPT_HTTPHEADER , $context['http']['header'] ) ;			// ヘッダー
 	$res1 = curl_exec( $curl ) ;
@@ -57,11 +86,71 @@ $request_url = 'https://api.twitter.com/2/users/'.$id.'/following';
 	$header = substr( $res1, 0, $res2['header_size'] ) ;		// レスポンスヘッダー (検証に利用したい場合にどうぞ)
  $array = json_decode($json,true);
 
- for($i=0;$i<99;$i++){
-  echo "@".$array['data'][$i]["username"]."<br>";
+ for($i=0;$i<100;$i++){
+
+	if(isset($array['data'][$i]['username'])){
+		echo "<a href ='https://twitter.com/".$array['data'][$i]['username']."'>".$array['data'][$i]['name']."</a><br>";
+	}
+}
+//var_dump($json);
+
+
+
+
+
+
+
+
+
+
+
+// パラメータ (オプション)
+$params = array(
+
+	//"usernames" => $_GET["user_id"],
+	//"max_results" => '10',
+	"pagination_token" => "7R04EU6M5K5HGZZZ",
+	//"next_token"  => 'U08O4JJEM3G1EZZZ',
+
+) ;
+
+// パラメータがある場合
+if( $params ) {
+	$request_url .= '&'.http_build_query( $params ) ;
 }
 
+// リクエスト用のコンテキスト
+$context = array(
+	'http' => array(
+		'method' => 'GET' , // リクエストメソッド
+		'header' => array(			  // ヘッダー
+			'Authorization: Bearer ' . $bearer_token ,
+		) ,
+	) ,
+) ;
 
+	// cURLを使ってリクエスト
+	$curl = curl_init() ;
+	curl_setopt( $curl , CURLOPT_URL , $request_url ) ;
+	curl_setopt( $curl , CURLOPT_HEADER, 1 ) ; 
+	var_dump($request_url);
+	curl_setopt( $curl , CURLOPT_RETURNTRANSFER , true ) ;								// curl_execの結果を文字列で返す
+	curl_setopt( $curl , CURLOPT_HTTPHEADER , $context['http']['header'] ) ;			// ヘッダー
+	$res1 = curl_exec( $curl ) ;
+	$res2 = curl_getinfo( $curl ) ;
+	curl_close( $curl ) ;
 
+	// 取得したデータ
+	$json = substr( $res1, $res2['header_size'] ) ;				// 取得したデータ(JSONなど)
+	$header = substr( $res1, 0, $res2['header_size'] ) ;		// レスポンスヘッダー (検証に利用したい場合にどうぞ)
+ $array = json_decode($json,true);
 
+ for($i=0;$i<10;$i++){
+
+	if(isset($array['data'][$i]['username'])){
+		echo "<a href ='https://twitter.com/".$array['data'][$i]['username']."'>21~".$array['data'][$i]['name']."</a><br>";
+	}
+}
+
+var_dump($json);
 
